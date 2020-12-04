032973ff17a096dba0652fd8825179d29db0e948
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-04 03:26:21 +0000

    bft and dft refactor

--- src/demos/traversals.py
+++ src/demos/traversals.py
@@ -86,3 +86,24 @@ def breadth_first_traversal(root):
             queue.append(node.right)
 
     return result
+
+
+def depth_first_traversal(root):
+    if root is None:
+        return []
+
+    result = []
+    stack = []
+    stack.append(root)
+
+    while len(stack) != 0:
+        node = stack.pop()
+
+        result.append(node.val)
+        if node.left is not None:
+            stack.append(node.left)
+
+        if node.right is not None:
+            stack.append(node.right)
+
+    return result
